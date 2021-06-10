088b88cbd65db0348ca55da3613a9d083910565f
Pedro Gordillo <pedrogordillo2510@gmail.com>
2021-06-10 10:58:56 -0600

    Codificacion ejercicio 9, cuando del par de temperaturas, 1 sea cero termina el programa y muestra el promedio

---
+++ Ejercicio9.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class Ejercicio9{
+
+
+	public static void main(String [] args){
+
+
+		Double promedio = 0.0;
+		Double totalTemp =0.0;
+		int contador =0;
+		Double tm1=0.0,tm2=0.0;
+
+		Scanner sc = new Scanner(System.in);
+	
+			
+
+		System.out.println("Ingresa la temperatura 1");
+		 tm1= sc.nextDouble();
+		 totalTemp =tm1;
+		 	System.out.println(totalTemp);
+
+		 if (tm1 !=0) {
+
+		
+	
+			 while (tm1!=0) {
+
+			 	contador++;
+			 	
+				
+					System.out.println("Ingresa la temperatura 2");
+					tm2= sc.nextDouble();	
+					totalTemp = totalTemp + tm2;
+					System.out.println(totalTemp);
+	
+	
+					
+
+					System.out.println("Ingresa la temperatura 1");
+					tm1= sc.nextDouble();
+					totalTemp = totalTemp + tm1;
+					System.out.println(totalTemp);
+
+			}
+		
+			promedio = totalTemp/contador;
+
+
+			System.out.println("El promedio de las temperaturas es: " + promedio);
+		 	
+		 }
+
+		
+		 else{
+		 	System.out.println("Ingresa una temperatura distinta de 0");
+		 }
+
+
+	}
+
+
+} 
